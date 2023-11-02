f160221f0ab14b4603674bdd9543800d56c1468f
Arifin79 <arifin.nurhidayah1933@gmail.com>
2023-11-02 20:24:06 +0700

    Update login

--- login.php
+++ login.php
@@ -2,15 +2,22 @@
 <html>
   <head>
     <link rel="stylesheet" href="globals.css" />
-    <link rel="stylesheet" href="style/style_login.css" />
+    <link rel="stylesheet" href="style/style_login.css?v2" />
   </head>
   <body>
     <div class="login">
       <div class="frame">
         <div class="text-wrapper">Sign in</div>
-        <div class="overlap-group"><div class="div">Username</div></div>
-        <div class="overlap"><div class="div">Password</div></div>
-        <div id="loginDiv" class="div-wrapper"><div class="text-wrapper-2">Login</div></div>
+        <div class="overlap-group"> 
+          <input type="text" name="username" id="username" class="div" autofocus="true" required placeholder="Username">
+        </div>
+        <div class="overlap">
+          <input type="password" name="password" id="password" class="div" placeholder="Password" required>
+        </div>
+        <div id="loginDiv" class="div-wrapper">
+        <button type="submit" class="text-wrapper-2">Login</button>
+        </div>
+        
       </div>
       <div class="group">
         <div class="text-wrapper-3">WELCOME!</div>
